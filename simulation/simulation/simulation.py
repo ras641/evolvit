6745b496564c203044cce85d254f8b897f4f80c0
ras <r.tait64@protonmail.com>
2025-03-27 16:22:27 +1000

    no spawn

--- simulation/simulation/simulation.py
+++ simulation/simulation/simulation.py
@@ -33,14 +33,6 @@ def initialize_creatures(world):
         creature = Creature(position=pos, organs=organs, name=name)
         cell.add(creature, log_spawn=False)
 
-    c([150, 250], [
-        {"type": "mouth", "position": [-30, -30], "size": 10},
-        {"type": "flipper", "position": [-30, 30], "size": 5},
-        {"type": "eye", "position": [30, -30], "size": 10},
-        {"type": "spike", "position": [30, 30], "size": 10}
-        ]
-    , "Xman")
-
 def start_simulation():
     print(f"✅ cell_grid initialized with size {len(world.cell_grid)}x{len(world.cell_grid[0])}")
     threading.Thread(target=simulation_loop, daemon=True).start()
